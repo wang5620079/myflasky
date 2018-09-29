df2ebbba2fc7f91fb20d2b47d159480a7c01f055
win-pc <wang5620079@163.com>
2018-09-29 22:17:14 +0800

    第8章结束

--- app/models.py
+++ app/models.py
@@ -9,7 +9,14 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean,default=False,index=True)
+    permissions=db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
 
     def __repr__(self):
         return '<Role %r>' % self.name
