97c22e81bf35626f28ac33cb63821a91a610a738
wang5620079 <wang5620079@163.com>
2018-09-30 11:27:50 +0800

    第10章开始，增加ping

--- app/models.py
+++ app/models.py
@@ -165,6 +165,10 @@ class User(UserMixin, db.Model):
     def is_administrator(self):
         return self.can(Permission.ADMIN)
 
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
     def __repr__(self):
         return '<User %r>' % self.username
 
